6783edbd35ccab549b525f51fc37af634525f5d2
Vivien60 <phedras.ch@gmail.com>
2026-04-13 19:32:06 +0200

    Chore: init entities and refacto conf
    OSP-6 créer les entités
    Comprend la création de la BDD et de sa structure

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # Formation_OC_Symfony_P13_ecommerce-And-API
+
+
+```mermaid
+classDiagram
+direction LR
+    class Cart {
+        totalPrice
+        createdAt
+        updatedAt
+    }
+
+    class Order {
+        totalPrice
+        createdAt
+        updatedAt
+    }
+    
+    class User {
+	    email
+	    firstname
+	    lastname
+	    password
+	    cguAccepted [yes, no]
+	    api [yes, no]
+        createdAt
+        updatedAt
+    }
+
+    class Product {
+        name
+        price
+        shortDescription
+        description
+        image
+        createdAt
+        updatedAt
+    }
+
+    class OrderItem {
+        quantity
+        unitPrice
+        createdAt
+    }
+
+    class CartItem {                                                                                                                                                                          
+        quantity
+        createdAt
+    } 
+
+    User "1" -- "1" Cart
+    User "1" -- "*" Order
+    Order "1" -- "*" OrderItem
+    OrderItem "*" -- "1" Product
+    Cart "1" -- "*" CartItem
+    CartItem "*" -- "1" Product
+```
---
+++ src/Entity/Order.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrderRepository::class)]
+#[ORM\Table(name: '`order`')]
+class Order
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?float $totalPrice = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'orders')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $owner = null;
+
+    /**
+     * @var Collection<int, OrderItem>
+     */
+    #[ORM\OneToMany(targetEntity: OrderItem::class, mappedBy: 'purchaseOrder', orphanRemoval: true, cascade: ['persist', 'remove'])]
+    private Collection $items;
+
+    public function __construct()
+    {
+        $this->items = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTotalPrice(): ?float
+    {
+        return $this->totalPrice;
+    }
+
+    public function setTotalPrice(float $totalPrice): static
+    {
+        $this->totalPrice = $totalPrice;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getOwner(): ?User
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(?User $owner): static
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, OrderItem>
+     */
+    public function getItems(): Collection
+    {
+        return $this->items;
+    }
+
+    public function addItem(OrderItem $item): static
+    {
+        if (!$this->items->contains($item)) {
+            $this->items->add($item);
+            $item->setPurchaseOrder($this);
+        }
+
+        return $this;
+    }
+
+    public function removeItem(OrderItem $item): static
+    {
+        if ($this->items->removeElement($item)) {
+            // set the owning side to null (unless already changed)
+            if ($item->getPurchaseOrder() === $this) {
+                $item->setPurchaseOrder(null);
+            }
+        }
+
+        return $this;
+    }
+}
