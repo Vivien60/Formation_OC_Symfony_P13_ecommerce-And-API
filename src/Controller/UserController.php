3cc937610aa32d62186fe68bfdd32f043602d334
Vivien60 <phedras.ch@gmail.com>
2026-04-15 23:13:05 +0200

    Feat : lister les commandes #23

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Repository\OrderRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,12 +10,13 @@ use Symfony\Component\Routing\Attribute\Route;
 final class UserController extends AbstractController
 {
     #[Route('/account', name: 'app_user')]
-    public function index(Service\GetOrders $getOrders): Response
+    public function index(UserRepository $userRepository): Response
     {
-        $orders = $getOrders->allWithPagination(page: 1, maxPerPage: 9);
+        $user = $userRepository->find(342);
+
         return $this->json(
-            data:['orders' => $orders, $orders],
-            context:['groups' => ['order:read']]
+            data:['orders' => $user->getOrders()],
+            context:['groups' => ['user:read']]
         );
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
