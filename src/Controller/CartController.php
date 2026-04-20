d3db240b0ed1d409f1fbedb67bf177f8b45a4f94
Vivien60 <phedras.ch@gmail.com>
2026-04-20 23:51:33 +0200

    Feat: vider un panier #16

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -16,5 +19,18 @@ final class CartController extends AbstractController
         return $this->render('cart/index.html.twig', [
             'controller_name' => 'CartController',
         ]);
+    }
+
+    #[Route('/cart/truncate', name: 'app_cart_truncate', methods: ['POST'])]
+    public function truncate(UserRepository $userRepository, EntityManagerInterface $manager)
+    {
+        $user = $this->getUser();
+        /**
+         * @var User $user
+         */
+        $user->getCart()->getItems()->clear();
+        $manager->flush();
+        $updatedUser = $userRepository->find($user->getId());
+        return $this->json(data: $updatedUser->getCart(), context: ['groups' => ['cart:read']]);
     }
 }
