77df99379ca5d868a1c800a8146d038aecc72a25
Vivien60 <phedras.ch@gmail.com>
2026-05-14 01:11:08 +0200

    Chore: Nettoyage

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -6,14 +6,10 @@
 let cardEnhancement = function () {
     document.addEventListener('click', function (ev) {
         const card = ev.target.closest('[data-component="card"]');
-        console.log('card');
-        console.log(card);
         if (!card || ev.redispatched) return;
         if (ev.target.closest('[data-click]')) return;
 
         const mainLink = card.querySelector('.card__link');
-        console.log('mainLink');
-        console.log(mainLink);
         if (ev.target === mainLink) return;
 
         const noTextSelected = !window.getSelection().toString();
